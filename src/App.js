c9e37da520fab376eb2ee905c03801ba7d95b634
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-09-12 23:00:23 +0900

    chat-ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Router from "./Router";
 import "./assets/reset.css";
 import "./assets/style.css";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <main>
+      <Header />
       <Router />
     </main>
   );
--- src/templates/Chat.js
+++ src/templates/Chat.js
@@ -1,7 +1,96 @@
 import React from "react";
+import { Scrollbars } from "react-custom-scrollbars";
+import { TextInput } from "../components/UIkit";
+import Button from "@material-ui/core/Button";
 
 const Chat = () => {
-  return <div>チャットルーム</div>;
+  const messages = [
+    { user_id: 1, content: "メッセージ1" },
+    { user_id: 2, content: "メッセージ2" },
+    { user_id: 3, content: "メッセージ3" },
+    { user_id: 3, content: "メッセージ3" },
+    { user_id: 3, content: "メッセージ3" },
+  ];
+  return (
+    <div className="flex">
+      <div className="chat-rooms-menu">
+        <Scrollbars>
+          <ul className="chat-room-list-container">
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+            <li className="chat-room-list">
+              <div className="chat-icon"></div>
+              <span className="chat-room-name">チャットルーム1</span>
+            </li>
+          </ul>
+        </Scrollbars>
+      </div>
+      <div className="chat-message-container">
+        <Scrollbars>
+          <ul>
+            {messages.map((message, index) => (
+              <div
+                className={
+                  message.user_id === 1
+                    ? "my-message-container"
+                    : "other-message-container"
+                }
+                key={index}
+              >
+                <span>{message.user_name}</span>
+                <div
+                  className={
+                    message.user_id === 1
+                      ? "my-message-content"
+                      : "other-message-content"
+                  }
+                >
+                  <span className="message-text">{message.content}</span>
+                </div>
+              </div>
+            ))}
+          </ul>
+        </Scrollbars>
+        <TextInput
+          fullWidth={true}
+          label={"メッセージを入力"}
+          multiline={true}
+          required={false}
+          rows={5}
+          // value={content}
+          type={"text"}
+          // onChange={inputPassword}
+        />
+        <Button>送信</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
