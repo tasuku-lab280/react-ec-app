e9252d0bc97d1f508b1cd74a4036bc49ac581b95
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-09-12 15:36:38 +0900

    firebase設定、ファイル整理

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEVd4CBxmMfQyrXMZTyQ1AXE-ety9C6Lg",
+  authDomain: "ec-app-6d36e.firebaseapp.com",
+  databaseURL: "https://ec-app-6d36e.firebaseio.com",
+  projectId: "ec-app-6d36e",
+  storageBucket: "ec-app-6d36e.appspot.com",
+  messagingSenderId: "773380016401",
+  appId: "1:773380016401:web:03d52939e37e0f1d45e4b0",
+  measurementId: "G-84RF551DV7",
+};
